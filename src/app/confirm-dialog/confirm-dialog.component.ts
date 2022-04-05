b08c0e6fd4d79cd6d2b24afb3a635f743bb2ebb9
cfho <cfho@users.noreply.github.com>
2022-04-05 13:50:52 +0800

    create files structures

---
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,43 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  Inject,
+  Output,
+} from '@angular/core';
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss'],
+})
+export class ConfirmDialogComponent {
+
+  value;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      cancelText: string;
+      confirmText: string;
+      message: string;
+      title: string;
+    },
+    private mdDialogRef: MatDialogRef<ConfirmDialogComponent>
+  ) {}
+  public cancel() {
+    this.close(false);
+  }
+  public close(value) {
+    this.mdDialogRef.close(value);
+  }
+  public confirm() {
+    this.close(true);
+  }
+  @HostListener('keydown.esc')
+  public onEsc() {
+    this.close(false);
+  }
+}
